09845452db121b8f678efeaab369d523ee39f1f9
Isain Rodriguez <alu2017440@aulas.local>
2019-02-27 20:16:28 +0100

    hecha coneccion arangoDB

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -5,10 +5,27 @@
  */
 package controller;
 
-/**
- *
- * @author alu2017440
- */
+import com.arangodb.ArangoCollection;
+import com.arangodb.ArangoCursor;
+import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.entity.CollectionEntity;
+import com.arangodb.model.AqlQueryOptions;
+import com.arangodb.util.MapBuilder;
+import com.arangodb.velocypack.VPackSlice;
+import com.arangodb.velocypack.exception.VPackException;
+import com.arangodb.velocypack.module.jdk8.VPackJdk8Module;
+
 public class Manager {
     
+    Connection connection = new Connection();
+    ArangoDB arangoDB;
+    
+    public Manager() {
+        arangoDB = new ArangoDB.Builder().host(connection.getIp(), connection.getPort())
+                .user(connection.getUser())
+                .password(connection.getPass())
+                .registerModule(new VPackJdk8Module()).build();
+    }
 }
